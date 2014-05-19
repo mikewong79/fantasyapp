e521469855816894716a97421edf63638ac21b62
Mike Wong <mikewong79@gmail.com>
2014-05-19 00:35:37 -0700

    added mailer info to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,11 +59,22 @@ Fantasyapp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  # Mailer Setup
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'google.com',
+  user_name:            'nba.fantasy.commish',
+  password:             'fantasycomm1sh',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
